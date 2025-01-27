4c63c32e691fbde840ddf5b580f8c52764a2160d
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-01-27 23:49:06 +0100

    feat: add react-compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+// @ts-expect-error - no TS types yet for beta test.
+import PluginObject from 'babel-plugin-react-compiler'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tailwindcss()],
+  plugins: [react(), TanStackRouterVite(), tailwindcss(), [PluginObject]],
 });
