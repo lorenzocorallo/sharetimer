32a1f0df7f67dadc3c1b77505ddf359abc7fff9e
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-01-28 19:56:37 +0100

    fix: show react-scan only in dev

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,15 @@
+import { scan as reactScan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { IdProvider } from "./context/id-context";
 import "./index.css";
 import { Router } from "./router";
+
+if (typeof window !== "undefined" && process.env.NODE_ENV === "development") {
+  reactScan({
+    enabled: true,
+  });
+}
 
 // Render the app
 const rootElement = document.getElementById("root")!;
